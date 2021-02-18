b1396af396cd3a0c309be56679df597c47a83e55
ruiminxu <64786097+ruiminxu@users.noreply.github.com>
2021-02-18 17:03:52 -0500

    Update PidManager.c

--- PidManager.c
+++ PidManager.c
@@ -4,11 +4,15 @@
 int allocate_map()
 {
     if(pid_map == NULL)  return -1;
-
+    //If the array is NULL return -1
     for(int i = 0; i < MAX_PID - MIN_PID; i++)
     {
         pid_map[i] = 0;
     }
+    /*
+        The for loop initialize each of the index with 0
+        Meaning it is each index is empty and ready for storing
+     */
 
     return 1;
 }
@@ -16,7 +20,7 @@ int allocate_map()
 int allocate_pid()
 {
     if(pid_map == NULL) return -1;
-
+    //If the array is NULL return -1
     for(int i = 0; i < MAX_PID - MIN_PID; i++)
     {
         if(pid_map[i] == 0)
@@ -25,25 +29,32 @@ int allocate_pid()
             return MIN_PID + i;
         }
     }
+    /*
+       The for loop will look for an empty index
+       if it is empty, it will initialize it as 1
+       then return the pid number
+    */
 }
 
 void release_pid(int pid)
 {
-    if(pid_map == NULL)
+    if(pid_map == NULL)  //If the array is NULL, print the error message
     {
         printf("There is nothing to realease");
-    }else if(pid < MIN_PID || pid > MAX_PID)
+    }else if(pid < MIN_PID || pid > MAX_PID)  //If the pid is not in range, print the error message
     {
         printf("pid is not in range");
     }else{
-        int newPID = pid - MIN_PID;
+        int newPID = pid - MIN_PID; //to find the index of the pid, do pid - MIN-PID
 
-        if(pid_map[newPID] == 0)
+        if(pid_map[newPID] == 0)  //If the index of the pid is already empty, print error message
         {
             printf("There is nothing to release");
         }else{
-            printf("released pid: %d \n", pid);
+            printf("released pid: %d \n", pid);  //If it is not empty, release the pid from the index
+
             pid_map[newPID] = 0;
         }
     }
+}
 }
